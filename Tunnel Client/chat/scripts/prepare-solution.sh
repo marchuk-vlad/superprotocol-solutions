0f7743476100e0cac352c348a46172745f3051ec
Vlad Marchuk <marchukvl02@gmail.com>
2023-12-08 23:05:17 +0300

    Fixed chat deploy script

--- Tunnel Client/chat/scripts/prepare-solution.sh
+++ Tunnel Client/chat/scripts/prepare-solution.sh
@@ -5,7 +5,22 @@ solution_path=$1
 solution_name=$2
 
 # cd to one directory up of current script
-cd $(dirname $(realpath "$0"))/..
+#!/bin/bash
+set -e
+
+solution_path=$1
+solution_name=$2
+
+# cd to one directory up of current script
+REAL_PATH=$(realpath "$0")
+LOCAL_PATH=$(dirname "$REAL_PATH")
+cd "$LOCAL_PATH"
+cd ..
+
+npm ci
+npm run build
+cp -R .next dist node_modules public next.config.js package.json .env $solution_path
+
 
 npm ci
 npm run build

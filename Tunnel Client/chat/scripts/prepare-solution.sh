88268bc26127173cdbfecdac50c73a1031e8ffc4
Vlad Marchuk <marchukvl02@gmail.com>
2023-12-08 23:29:01 +0300

    Return old script

--- Tunnel Client/chat/scripts/prepare-solution.sh
+++ Tunnel Client/chat/scripts/prepare-solution.sh
@@ -5,22 +5,7 @@ solution_path=$1
 solution_name=$2
 
 # cd to one directory up of current script
-#!/bin/bash
-set -e
-
-solution_path=$1
-solution_name=$2
-
-# cd to one directory up of current script
-REAL_PATH=$(realpath "$0")
-LOCAL_PATH=$(dirname "$REAL_PATH")
-cd "$LOCAL_PATH"
-cd ..
-
-npm ci
-npm run build
-cp -R .next dist node_modules public next.config.js package.json .env $solution_path
-
+cd $(dirname $(realpath "$0"))/..
 
 npm ci
 npm run build
